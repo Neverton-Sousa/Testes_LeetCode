04deede92785c1f98de106df014eea1b9c989d30
Neverton Sousa <nevertonsousa2@gmail.com>
2024-04-12 23:23:52 -0300

    teste_12

---
+++ diarios/teste_12.py
@@ -0,0 +1,20 @@
+def trap(height):
+    i = 0
+    left_max = height[0]
+    sum = 0
+    j = len(height) - 1
+    right_max = height[j]
+    while i < j:
+        if left_max <= right_max:
+            sum += left_max - height[i]
+            i += 1
+            left_max = max(left_max, height[i])
+        else:
+            sum += right_max - height[j]
+            j -= 1
+            right_max = max(right_max, height[j])
+    return sum
+
+
+print(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+print(trap([4, 2, 0, 3, 2, 5]))
