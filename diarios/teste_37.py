ae49158ae957818949da4611c5893674c3c3bb25
Neverton Sousa <nevertonsousa2@gmail.com>
2024-05-17 22:38:24 -0300

    teste_37

---
+++ diarios/teste_37.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        def countSteps(root):
+            if(root == None):
+                return 0
+            leftCoins=countSteps(root.left)
+            rightCoins=countSteps(root.right)
+            self.ans=abs(leftCoins)+abs(rightCoins)
+            return root.val+leftCoins+rightCoins-1
+        countSteps(root)
+        return self.ans
